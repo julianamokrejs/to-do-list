af3b1d166d4a6211c7ff31ff07ba835971b4d0a9
julianamokrejs <ju.mokrejs@gmail.com>
2018-09-27 14:41:17 -0300

    ajustes no js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
 const tarefaComposerButton = document.querySelector(".tarefa-composer__button");
 const tarefaComposerInput = document.getElementById("tarefaComposerInput");
 const listaTarefas = document.querySelector(".lista-tarefas");
-const listaTarefasItem = document.querySelector(".lista-tarefas__item");
 
 tarefaComposerButton.addEventListener("click", function (event) {
     event.preventDefault();
@@ -13,12 +12,13 @@ tarefaComposerButton.addEventListener("click", function (event) {
         return false;
     }
 
+
     const divTarefas = document.createElement("div");
 
     const checkboxTarefaButton = document.createElement("div");
     checkboxTarefaButton.className = "item__checkbox";
     checkboxTarefaButton.innerHTML = `<div class="tarefas-lista">
-    <div class="lista-tarefas__item">
+    <div class="lista-tarefas__item" id="target "ondrop="drop_handler(event);" ondragover="dragover_handler(event);">
         <div class="lista-tarefas__item-checkbox">
             <input class="lista-tarefas__checkbox" type="checkbox">
             <span class="lista-tarefas__item-texto">${tarefaComposerInput.value}</span>
@@ -27,6 +27,26 @@ tarefaComposerButton.addEventListener("click", function (event) {
     </div>
 </div>`
 
+
+    const socorroMeuDeus = document.querySelector(".lista-tarefas__item");
+
+    // socorroMeuDeus.addEventListener("dragStart", function dragstart_handler(ev) {
+    // console.log("dragStart");
+    // // Adiciona o id do elemento em questão ao objeto de transferência de dados (dataTransfer)
+    // ev.dataTransfer.setData(".listaTarefasItem", ev.target.id);
+    // ev.dataTransfer.dropEffect = "move";
+    // })
+    // function dragover_handler(ev) {
+    //     ev.preventDefault();
+    //     // Define o dropEffect para ser do tipo move
+    //     ev.dataTransfer.dropEffect = "move"
+    //    }
+    // function drop_handler(ev) {
+    //     ev.preventDefault();
+    //     // Pega o id do alvo e adiciona o elemento que foi movido para o DOM do alvo
+    //     var data = ev.dataTransfer.getData(".listaTarefasItem");
+    //     ev.target.appendChild(document.getElementById(data));
+    //    }
 
     divTarefas.appendChild(checkboxTarefaButton);
     listaTarefas.appendChild(divTarefas);
@@ -62,6 +82,41 @@ tarefaComposerButton.addEventListener("click", function (event) {
       }
       
 })
+
+// listaTarefasItem.onmousedown = function (event) { // (1) start the process
+
+//     // (2) prepare to moving: make absolute and top by z-index
+//     listaTarefasItem.style.position = 'absolute';
+//     listaTarefasItem.style.zIndex = 1000;
+//     // document.body.appendChild(listaTarefasItem);
+  
+//     // ...and put that absolutely positioned listaTarefasItem under the cursor
+//     moveAt(event.pageX, event.pageY);
+  
+//     // centers the listaTarefasItem at (pageX, pageY) coordinates
+//     function moveAt(pageX, pageY) {
+//       listaTarefasItem.style.left = pageX - listaTarefasItem.offsetWidth / 2 + 'px';
+//       listaTarefasItem.style.top = pageY - listaTarefasItem.offsetHeight / 2 + 'px';
+//     }
+  
+//     function onMouseMove(event) {
+//       moveAt(event.pageX, event.pageY);
+//     }
+  
+//     // (3) move the listaTarefasItem on mousemove
+//     document.addEventListener('mousemove', onMouseMove);
+  
+//     // (4) drop the listaTarefasItem, remove unneeded handlers
+//     listaTarefasItem.onmouseup = function () {
+//       document.removeEventListener('mousemove', onMouseMove);
+//       listaTarefasItem.onmouseup = null;
+//     };
+  
+//   };
+  
+//   listaTarefasItem.ondragstart = function () {
+//     return false;
+//   };
 
 })
 
